cf426710a7be768f24e4957d9ced38ff1154c9f6
barbads <matheusbmiranda@gmail.com>
2017-06-26 11:30:12 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -1,5 +1,6 @@
 from socket import *
 from thread import *
+from time import *
 import sys
 
 def IniciaSockets():	#Funcao que inicia sockets e comeca a ouvir requisicoes do browser
@@ -11,13 +12,16 @@ def IniciaSockets():	#Funcao que inicia sockets e comeca a ouvir requisicoes do 
 	except Exception, ErroSocket:
 		print("Socket nao foi iniciado: ", ErroSocket)
 		sys.exit(2)
-
+	except KeyboardInterrupt:
+		printf("Interrupcao do usuario.")
+		sys.exit(1)
+			
 	while 1:
 		try:
+			resposta = ""
 			conn, addr = server.accept()
 			print("Conexao aceita.")
 			resposta = conn.recv(1024)
-			print("Houve resposta!: ", resposta)
 			start_new_thread(Tratamento, (conn, addr, resposta))
 		except Exception, ErroConn:
 			print("Nao houve resposta ou a conexao nao foi aceita: ", ErroConn)
@@ -29,12 +33,29 @@ def IniciaSockets():	#Funcao que inicia sockets e comeca a ouvir requisicoes do 
 
 def Tratamento(conn, addr, resposta):
 	try:
+		host = ""
+		aux = ""
 		aux = resposta.split('\r')[0]
 		link = aux.split(' ')
-		url = link[1]
-		print(url)
-		flags = VerificaArquivos(resposta, url)
-		RespostaProxy(conn, addr, resposta, flags)
+		aux2 = link[1]
+		i = aux2.find("://") # requisicoes http:// ou https://
+		if (i == -1):
+			aux3 = aux2
+		else:
+			aux3 = aux2[(i+3):]
+		j = aux3.find(":")
+		if j==-1:
+			aux4 = aux3
+		else:
+			aux4 = aux3[:j]
+		k = aux4.find("/")
+		if k == -1:
+			host=aux4
+		else:
+			host = aux4[:k]
+		print(host)
+		flags = VerificaArquivos(conn, resposta, host)
+		RespostaProxy(conn, addr, resposta, flags, host)
 	except Exception, ErroString:
 		print("Erro no tratamento da resposta: ", ErroString)
 		sys.exit(2)
@@ -42,34 +63,25 @@ def Tratamento(conn, addr, resposta):
 		print("Interrupcao do usuario.")
 		sys.exit(1)
 
-def VerificaArquivos(resposta, url):
+def VerificaArquivos(conn, resposta, host):
 	try:
 		fobject = open("blacklist.txt", "r")  #blacklist: tratamento
-		proibidos = fobject.readlines()
-		if url in proibidos:
+		proibidos = fobject.read().splitlines()
+		print (proibidos)
+		if host in proibidos:
 			blacklisturl = 1
 		else:
 			blacklisturl = 0
 		fobject.close()
 
 		fobject = open("whitelist.txt", "r")
-		permitidos = fobject.readlines()
-		print(permitidos)
-		if url in permitidos:
+		permitidos = fobject.read().splitlines()
+		if host in permitidos:
 			whitelisturl = 1
 		else:
 			whitelisturl = 0
 		fobject.close()
-		
-		fobject = open("denyterms.txt", "r")
-		termos = fobject.readlines()
-		print(termos)
-		termos = ['BLACKLIST_DOMAINS']
-		for i in range(0, len(config['BLACKLIST_DOMAINS'])):
-    		if config['BLACKLIST_DOMAINS'][i] in url:
-        		conn.close()
-		return	
-		
+
 		return((blacklisturl,whitelisturl))	
 	except Exception, ErroArquivos:
 		print("Houve um erro no gerenciamento de arquivos de permissoes: ", ErroArquivos)
@@ -77,8 +89,3 @@ def VerificaArquivos(resposta, url):
 	except KeyboardInterrupt:
 		print("Interrupcao do usuario.")
 		sys.exit(1)
-
-def RespostaProxy(conn, addr, resposta, flags)
-
-	
-IniciaSockets()
