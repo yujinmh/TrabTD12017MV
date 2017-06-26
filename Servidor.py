6ba25a6bee6a56efaab593195a225db6bff8d166
Victor Yuji Sato <vyujisato@gmail.com>
2017-06-26 03:38:19 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -60,7 +60,7 @@ def VerificaArquivos(resposta, url):
 		else:
 			whitelisturl = 0
 		fobject.close()
-
+		
 		return((blacklisturl,whitelisturl))	
 	except Exception, ErroArquivos:
 		print("Houve um erro no gerenciamento de arquivos de permissoes: ", ErroArquivos)
@@ -70,6 +70,41 @@ def VerificaArquivos(resposta, url):
 		sys.exit(1)
 
 def RespostaProxy(conn, addr, resposta, flags)
-	
+
+def proxy_server(temp, webserver, port, conn, addr, data):  
+    ip_host=socket.gethostbyname(webserver)   
+    consite=httplib.HTTPSConnection(webserver)
+    consite.request("GET","/")
+    r1=consite.getresponse()
+    print "\n[+] HOST:", webserver
+    print "[+] HOST_IP:", ip_host
+    print "[+] STATUS: ", r1.status, r1.reason
+    consite.close() 
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((webserver, port))
+        s.send(data)
+        if re.match(".*youtube.*", temp):    
+            print "\n[!] YOUTUBE: ", temp
+            html='Acesso nao autorizado!'
+            conn.send(html)
+            conn.close()
+            s.send(html)
+            s.close()
+        while True:
+            reply = s.recv(4096)
+            if (len(reply)>0):
+                conn.send(reply)
+                conn_server(reply)
+            else:
+                break
+        s.close()
+        conn.close()
+
+    except socket.error, (value, message):
+        s.close()
+        conn.close()
+        sys.exit(2)
+
 	
 IniciaSockets()
